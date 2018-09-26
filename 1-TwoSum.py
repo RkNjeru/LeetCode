6f3c0c0fd1eeb56e3acb7f4a8be764beba2221f9
RkNjeru <35582018+RkNjeru@users.noreply.github.com>
2018-09-25 20:29:55 -0400

    Completed Two Sum

--- 1-TwoSum.py
+++ 1-TwoSum.py
@@ -1,6 +1,8 @@
-##Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+##Given an array of integers, return indices of the two numbers such that they
+## add up to a specific target.
 ##
-##You may assume that each input would have exactly one solution, and you may not use the same element twice.
+##You may assume that each input would have exactly one solution, and you may
+## not use the same element twice.
 ##
 ##Example:
 ##
@@ -16,4 +18,20 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        
+
+##        Ideal solution:         
+##        for i in range(0, len(nums)-1):
+##            for j in range(i+1, len(nums)):
+##                if(nums[i] + nums[j] == target):
+##                    return [i, j]
+##                    
+
+
+##      Solution for time complexity on Leetcode 
+        for i in range(0, len(nums)):
+            try:
+                j = nums.index(target - nums[i])
+                return [i, j]
+            except:
+                continue
+            
