f1abe685b0243cdeb44c6a89be02c73ca797559e
RkNjeru <35582018+RkNjeru@users.noreply.github.com>
2018-12-28 13:02:04 +0300

    Progress on problem 2, traversed both lists and have output as number, not as linked list

--- 2-AddTwoNumbers.py
+++ 2-AddTwoNumbers.py
@@ -1,6 +1,9 @@
-##You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+##You are given two non-empty linked lists representing two non-negative 
+# integers. The digits are stored in reverse order and each of their 
+# nodes contain a single digit. Add the two numbers and return it as a linked list.
 ##
-##You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+##You may assume the two numbers do not contain any leading zero, 
+# except the number 0 itself.
 ##
 ##Example:
 ##
@@ -10,16 +13,42 @@
 
 
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
+class Solution:
     def addTwoNumbers(self, l1, l2):
         """
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
         """
+        n1 = ""
+        n2 = ""
+        pTemp = l1
+        while(pTemp != None):
+            n1 = str(pTemp.val) + n1
+            pTemp = pTemp.next
+
+        pTemp = l2 
+        while(pTemp != None):
+            n2 = str(pTemp.val) + n2
+            pTemp = pTemp.next
+
+        num1 = int(n1)
+        num2 = int(n2)
+
+        sum = num1 + num2
+        
+        
+        string1 = str(num1)
+        string2 = str(num2)
+        
+        
+
+    
+    
+
         
