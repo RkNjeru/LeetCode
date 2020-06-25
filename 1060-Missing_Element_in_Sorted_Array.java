94a9cbc3cc4b5a023b4b537abad2dde10dc8965b
Roy Njeru <roynjeru24@gmail.com>
2020-06-25 19:57:59 -0400

    1060 slightly faster

--- 1060-Missing_Element_in_Sorted_Array.java
+++ 1060-Missing_Element_in_Sorted_Array.java
@@ -1,6 +1,6 @@
 class MissingElemSolution{
 
-    public int missingElement(int[] nums, int k) {
+    public int missingElementOld(int[] nums, int k) {
         
         int arrayIndex = 0, curNumber = 0;
         int logicalCounter = nums[0];
@@ -22,6 +22,32 @@ class MissingElemSolution{
         return logicalCounter - 1;
     }
 
+    public int missingElement(int[] nums, int k) {
+        
+        for(int i = 0; i < nums.length - 1; i++){
+            int between = nums[i + 1] - nums[i] - 1;
+            
+            if(between == 0){
+                continue;
+            }
+            else{
+                k = k - between;
+            }
+            if(k == 0){
+                return nums[i] + between;
+            }
+            else if( k < 0){
+                int ans = nums[i] + between + k;
+                return ans;
+            }
+            
+        }
+        if(k > 0){
+            return nums[nums.length - 1] + k;
+        }
+        return -1;
+    }
+
     public static void main(String[] args){
         MissingElemSolution classObj = new MissingElemSolution();
         int[] ex1 = {4,7,9,10};
@@ -30,6 +56,6 @@ class MissingElemSolution{
         int k2 = 3; 
         int[] ex3 = {1, 2, 4};
         int k3 = 3;
-        System.out.println(classObj.missingElement(ex1, k1));
+        System.out.println(classObj.missingElement(ex3, k3));
     }
 }
