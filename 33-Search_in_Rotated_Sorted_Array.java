fc4d325680d6a60af18b4a91ecb448da59c1b2a7
Roy Njeru <roynjeru24@gmail.com>
2020-06-30 19:48:16 -0400

    33 added, WIP

---
+++ 33-Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,24 @@
+class SearchRotatedArraySolution{
+
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length, mid = 0;
+
+        while(low <= high){
+            mid = low + ((high - low) / 2);
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[low] <= target && nums[mid] > target){
+                high = mid - 1;
+            }
+            else if(nums[high] >= target && nums[mid] > target){
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+    
+    public static void main(String[] args){
+
+    }
+}
