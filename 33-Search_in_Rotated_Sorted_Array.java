20c9587ab73e7f2d79a6bb42e50fa9e40980dd1b
Roy Njeru <roynjeru24@gmail.com>
2020-07-08 19:04:46 -0400

    33 all test cases passed

--- 33-Search_in_Rotated_Sorted_Array.java
+++ 33-Search_in_Rotated_Sorted_Array.java
@@ -1,15 +1,54 @@
 class SearchRotatedArraySolution{
 
     public int search(int[] nums, int target) {
-        int low = 0, high = nums.length - 1, mid = 0;
+        int low = 0, high = nums.length - 1, mid = 0, pivot = 0;
+
+        if(nums == null || nums.length == 0){
+            return -1;
+        }
 
         // explanation: https://www.youtube.com/watch?v=QdVrY3stDD4
         
         // 1. find pivot (smallest element)
+        while(low < high){
+            mid = low + ((high - low) / 2);
+            if(nums[mid] > nums[high]){
+                low = mid + 1;
+            }
+            else{
+                high = mid;
+            }
+        }
+
+        pivot = low;
+        // System.out.println(pivot);
+        low = 0;
+        high = nums.length - 1;
+        
 
         // 2. determine which side of pivot target exists
 
+        if(nums[pivot] <= target && nums[high] >= target){
+            low = pivot;
+        }
+        else{
+            high = pivot - 1;
+        }
+
         // 3. binary search on know correct portion of array
+
+        while(low <= high){
+            mid = low + ((high - low) / 2);
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] > target){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
 
         return -1;
     }
@@ -23,6 +62,9 @@ class SearchRotatedArraySolution{
 
         int[] ex3 = {4, 5, 6, 7, 8, 1, 2, 3};
         int tar3 = 8;
-        System.out.println(classObj.search(ex3, tar3));
+
+        int[] ex4 = {5, 1, 2, 3, 4};
+        int tar4 = 1;
+        System.out.println(classObj.search(ex4, tar4));
     }
 }
